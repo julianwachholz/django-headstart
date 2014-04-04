e4ac8013b7869481bcff78d628a2e43b688920af
Julian Wachholz <julian@wachholz.ch>
2014-04-04 17:46:14 +0200

    Deprecate url patterns
    The patterns function will be deprecated as of Django 1.8 and removed in 2.0.
    
    See https://github.com/django/django/commit/d73d0e071c1b4c86d57994a0ab55a74cfe80cdf5

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,13 +1,12 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 from .views import IndexView
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
     #url(r'^accounts/', include('apps.accounts.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+]
